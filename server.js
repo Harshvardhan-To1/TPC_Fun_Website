52cc60354ad18b0f4af5e4d42df814a0bb13e579
Harshvardhan Choudhary <Harshvardhan.yashvardhan@gmail.com>
2025-11-06 17:04:31 +0530

    added eevreything working

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/chatbot', async (req, res) => {
   const userMessage = req.body.message;
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp"});
     const result = await model.generateContent(userMessage);
     const response = await result.response;
     const text = response.text();
